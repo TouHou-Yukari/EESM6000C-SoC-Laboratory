6b44d0e7dbd7c5db95472b01b12295ed26888ec4
TouHou-Yukari <halo11310926@live.hk>
2025-05-06 20:00:28 +0800

    Add files via upload

--- lab4_2/testbench/counter_la_fir/fir.c
+++ lab4_2/testbench/counter_la_fir/fir.c
@@ -2,11 +2,25 @@
 
 void __attribute__ ( ( section ( ".mprjram" ) ) ) initfir() {
 	//initial your fir
+	for (int i=0; i<N; i++) inputbuffer[i] = 0;
+	for (int i=0; i<N; i++) outputsignal[i] = 0;
 }
 
 int* __attribute__ ( ( section ( ".mprjram" ) ) ) fir(){
 	initfir();
 	//write down your fir
+	int sum;
+	int getData;
+	for (int i=0; i<N; i++) {
+	    sum = 0;
+		getData = inputsignal[i];
+		inputbuffer[i] = getData;
+		for (int j=0; j<i+1; j++) {
+			sum += inputbuffer[i-j] * taps[j];
+		}
+		outputsignal[i] = sum;
+	}
+	
 	return outputsignal;
 }
 		
